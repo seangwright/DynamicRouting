e5352d1cb695987fc1bbde95cd8b80ccf04d2ab1
Sean Wright <sean@seangwright.me>
2019-10-14 21:36:16 -0400

    feat(MVC): clean up packages, add types from KenticoBoilerplate_v12

---
+++ DynamicRouting.Kentico.MVC/DynamicRouteConstraint.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using CMS.Helpers;
+
+using RequestContext = System.Web.Routing.RequestContext;
+
+namespace DynamicRouting.Kentico.MVC
+{
+    public class DynamicRouteConstraint : IRouteConstraint
+    {
+        public DynamicRouteConstraint()
+        {
+        }
+
+        public bool Match(HttpContextBase httpContext, Route route, string parameterName, RouteValueDictionary values, RouteDirection routeDirection)
+        {
+            // Check if the controller is found and has the KMVCRouteOverPathPriority attribute.
+            string ControllerName = (values.ContainsKey("controller") ? values["controller"].ToString() : "");
+            return CacheHelper.Cache(cs => {
+                // Check if the Route that it found has the override
+                var factory = ControllerBuilder.Current.GetControllerFactory();
+                try
+                {
+                    var Controller = factory.CreateController(new RequestContext(httpContext, new RouteData(route, null)), ControllerName);
+                    return Attribute.GetCustomAttribute(Controller.GetType(), typeof(DynamicRouteConstraint)) != null;
+                }
+                catch (Exception)
+                {
+                    return false;
+                }
+            }, new CacheSettings(1440, "KMVCRouteOverPathPriority", ControllerName));
+        }
+
+    }
+}
